ea6cd91d4505987b5384aa31fd2a3bf9a8c42ba9
Buay Dak <buaypd@gmail.com>
2026-04-23 11:55:32 -0700

    Created DynamicStringList Object

--- DynamicStringList.java
+++ DynamicStringList.java
@@ -1,5 +1,24 @@
 
-public class DynamicStringList  {
+public class DynamicStringList implements StringList {
 
+    public int index;
+    public String[] myArr;
+    public String[] myNewArr;
+    public String value;
+
+
+
+    public DynamicStringList(int index, String[] myArr, String[] myNewArr, String value){
+        this.index = index;
+        this.myArr = myArr;
+        this.myNewArr = myNewArr;
+        this.value = value;
+    }
+
+    public String get(int index){
+    for (int i = 0; i < myArr.length; i++) {
+        }
+        return myArr[index];
+    }
     
 }
